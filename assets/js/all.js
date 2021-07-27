0d6beee7a56191ef229f5743cb37d6c762b7bb4a
rousong <recall131477@gmail.com>
2021-07-27 11:22:47 +0800

    Update 2021-07-27T03:22:36.431Z

---
+++ assets/js/all.js
@@ -0,0 +1,22 @@
+"use strict";
+
+(function ($) {
+  var changeItem = function changeItem() {
+    // 切換product-tab__item顯示不同product__content內容
+    var index = 0;
+    $('.product-tab__item').eq(index).addClass('is-active');
+    $('.product__content').eq(index).show();
+    $('.product-tab__item').on('click', function () {
+      index = $(this).index();
+      $('.product__content').hide();
+      $('.product__content').eq(index).fadeIn();
+      $('.product-tab__item').removeClass('is-active');
+      $('.product-tab__item').eq(index).addClass('is-active');
+    });
+  };
+
+  $(function () {
+    changeItem();
+  });
+})(jQuery);
+//# sourceMappingURL=all.js.map
